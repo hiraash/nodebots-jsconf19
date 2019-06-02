115d4d3d39b82cde1117aa921f3b3800451c0fd4
Pasindu De Silva <pasindud@users.noreply.github.com>
2019-06-02 22:12:38 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@
 npm install -g nodebots-interchange
 
 
-cd simplebot && npm install
-cd node-pixel && npm install
-cd johnny-five && npm install
+cd simplebot && npm install && cd ../
+cd node-pixel && npm install && cd ../
+cd johnny-five && npm install && cd ../
